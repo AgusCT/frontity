d7e9b1798e3708bcdab5691389fcad7b7170cc9b
Uche Jude <iamuchejude@gmail.com>
2020-03-03 02:09:52 +0100

    Write e2e tests for tiny-router

--- e2e/project/frontity.settings.ts
+++ e2e/project/frontity.settings.ts
@@ -55,7 +55,17 @@ const settings: Settings = [
   },
   {
     name: "tiny-router",
-    packages: ["e2e-tiny-router"]
+    packages: [
+      "e2e-tiny-router",
+      {
+        name: "@frontity/tiny-router",
+        state: { router: { autoFetch: false } }
+      },
+      {
+        name: "@frontity/wp-source",
+        state: { source: { api: "https://test.frontity.io/wp-json" } }
+      }
+    ]
   }
 ];
 
